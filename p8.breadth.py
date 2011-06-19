bf7933809ad9ae708ff1c442821e66e3f39b8824
David Medina <davidmedina9@gmail.com>
2011-06-19 13:13:00 +0200

    Added time counter

--- p8.breadth.py
+++ p8.breadth.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from puzzle8 import Node
+from time import time
 
 if __name__ == '__main__':
 
     goal = Node('123804765')
     #TODO: Check parity errors
-    initial = Node('217860345')
+    initial = Node('127860345')
     opened = set([initial])
     closed = set()
     counter = 0
@@ -15,6 +16,7 @@ if __name__ == '__main__':
     print 'Initial: {0}'.format(initial.to_string())
     print 'Goal   : {0}'.format(goal.to_string())
     raw_input('Continue...')
+    time_init = time()
 
     while opened != set():
         counter += 1
@@ -23,7 +25,8 @@ if __name__ == '__main__':
         closed.add(node_to_expand)
         succ = []
         if node_to_expand.matrix == goal.matrix:
-            result = result.replace('not ', '') #:P
+            time_end = time()
+            result = 'Goal found in {0:.2f} seconds'.format(time_end - time_init)
             break;
         up, down, left, right = node_to_expand.expand()
         if up not in closed:
